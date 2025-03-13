d218c530f227c8bd8eb9bd76a7884804afe1a9df
jihyun25 <dlwlgus0525@gmail.com>
2025-03-13 18:38:27 +0900

    test

--- setupPackage.js
+++ setupPackage.js
@@ -6,9 +6,12 @@ const __dirname = path.resolve();
 function main() {
   const source = fs.readFileSync(__dirname + "/package.json").toString("utf-8");
   const sourceObj = JSON.parse(source);
+  sourceObj.scripts = {};
+  sourceObj.devDependencies = {};
   sourceObj.main = "./index.es.js";
   sourceObj.types = "./index.es.d.ts";
   sourceObj.module = "./index.es.js";
+  sourceObj.files = ["dist"];
   sourceObj.exports = {
     ".": {
       types: "./index.es.d.ts",
@@ -17,11 +20,9 @@ function main() {
       "./style.css": "./style.css",
     },
   };
-
   if (sourceObj.main.startsWith("/dist/")) {
     sourceObj.main = sourceObj.main.slice(5);
   }
-
   fs.writeFileSync(
     __dirname + "/dist/package.json",
     Buffer.from(JSON.stringify(sourceObj, null, 2), "utf-8")
